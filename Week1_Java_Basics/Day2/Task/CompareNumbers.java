20adfe90e209e55cdd69c1072022ca58b5017a2a
maroof2424 <maroof96965@gmail.com>
2025-10-03 13:46:43 +0500

    Week1 Day2

---
+++ Week1_Java_Basics/Day2/Task/CompareNumbers.java
@@ -0,0 +1,14 @@
+public class CompareNumbers {
+    public static void main(String[] args) {
+        int a = 25;
+        int b = 42;
+
+        if (a > b){
+            System.out.println(a + " is larger than " + b);
+        } else if (a < b){
+            System.out.println(b + " is larger than " + a);
+        } else {
+            System.out.println("Both numbers are equal");
+        }
+    }
+}
---
+++ Week1_Java_Basics/Day2/Task/IsEven.java
@@ -0,0 +1,12 @@
+
+public class IsEven {
+    public static void main(String[] args) {
+        int num = 7;
+
+        if (num % 2 == 0){
+            System.out.println(num + " is Even");
+        } else {
+            System.out.println(num + " is Odd");
+        }
+    }
+}
